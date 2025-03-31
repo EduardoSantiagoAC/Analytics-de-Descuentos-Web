871263b4d0ce17b1d63da3ab1c154623680b934c
EduardoSantiagoAC <engingamingsantiago@gmail.com>
2025-03-31 13:02:40 -0600

    xd

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // Conexión DIRECTA (sin variables de entorno)
-mongoose.connect("mongodb+srv://SantiagoAD:unlock255@cluster0.4m8lv.mongodb.net/PlataformaDescuentos?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://SantiagoAD:unlock255@cluster0.4m8lv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => console.log("Conectado a MongoDB"))
   .catch(err => console.error("Error:", err));
 
