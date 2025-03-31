c1af7ad8d8e1862e44744e5b27d50a0667f2a040
EduardoSantiagoAC <engingamingsantiago@gmail.com>
2025-03-31 13:10:16 -0600

    0

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+require('dotenv').config(); // Solo para desarrollo
 const mongoose = require('mongoose');
 
-// Conexión DIRECTA (sin variables de entorno)
-mongoose.connect("mongodb+srv://SantiagoAD:unlock255@cluster0.4m8lv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-  .then(() => console.log("Conectado a MongoDB"))
-  .catch(err => console.error("Error:", err));
-
-  
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("Conectado a MongoDB"))
+.catch(err => console.error("Error:", err));
