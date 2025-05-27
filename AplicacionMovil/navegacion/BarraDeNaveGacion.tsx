08851b659a0c5eef71c18bbd0e9dad735580171c
EduardoSantiagoAC <engingamingsantiago@gmail.com>
2025-05-27 13:31:24 -0600

    .

--- AplicacionMovil/navegacion/BarraDeNaveGacion.tsx
+++ AplicacionMovil/navegacion/BarraDeNaveGacion.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import HomeScreen from '../Screens/home';
+import RopaScreen from '../Screens/Ropa';
+import ElectronicaScreen from '../Screens/Electronica';
+import HogarScreen from '../Screens/Articuloshogar';
 import BusquedaScreen from '../Screens/Busqueda';
-// import HomeScreen from '../Screens/home';
-// import RopaScreen from '../Screens/Ropa';
-// import ElectronicaScreen from '../Screens/Electronica';
-// import HogarScreen from '../Screens/Articuloshogar';
 
 const Stack = createNativeStackNavigator();
 
 const MainNavigator = () => {
   return (
-    <Stack.Navigator initialRouteName="Buscar">
-      <Stack.Screen name="Buscar" component={BusquedaScreen} />
-      {/* <Stack.Screen name="Inicio" component={HomeScreen} />
+    <Stack.Navigator initialRouteName="Inicio">
+      <Stack.Screen name="Inicio" component={HomeScreen} />
       <Stack.Screen name="Ropa" component={RopaScreen} />
       <Stack.Screen name="Electrónica" component={ElectronicaScreen} />
-      <Stack.Screen name="Hogar" component={HogarScreen} /> */}
+      <Stack.Screen name="Hogar" component={HogarScreen} />
+      <Stack.Screen name="Buscar" component={BusquedaScreen} />
     </Stack.Navigator>
   );
 };
