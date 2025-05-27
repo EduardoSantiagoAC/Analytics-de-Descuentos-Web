b60612d60235d9d463d9603923a46a9bd1728cd4
EduardoSantiagoAC <engingamingsantiago@gmail.com>
2025-05-26 23:18:17 -0600

    .

---
+++ AplicacionMovil/Screens/BusquedaScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const BusquedaScreen = () => {
+  return (
+    <View>
+      <Text>Pantalla de búsqueda funcionando</Text>
+    </View>
+  );
+};
+
+export default BusquedaScreen;
