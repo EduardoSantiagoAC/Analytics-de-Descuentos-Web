7cc1d39efd45914c53764f95cab41374a111dbf3
EduardoSantiagoAC <engingamingsantiago@gmail.com>
2025-06-01 02:47:55 -0600

    .

--- AplicacionMovil/navegacion/BarraDeNaveGacion.tsx
+++ AplicacionMovil/navegacion/BarraDeNaveGacion.tsx
@@ -1,39 +1,23 @@
 import React from 'react';
-import { createBottomTabsNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import HomeScreen from '../Screens/home';
-import BusquedaScreen from '../Screens/Busqueda';
 import RopaScreen from '../Screens/Ropa';
 import ElectronicaScreen from '../Screens/Electronica';
 import HogarScreen from '../Screens/Articuloshogar';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import BusquedaScreen from '../Screens/Busqueda';
 
-const Tab = createBottomTabsNavigator();
+const Stack = createNativeStackNavigator();
 
 const MainNavigator = () => {
   return (
-    <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ color, size }) => {
-          let iconName: string;
-          if (route.name === 'Home') iconName = 'home';
-          else if (route.name === 'Buscar') iconName = 'search';
-          else if (route.name === 'Ropa') iconName = 'checkroom';
-          else if (route.name === 'Electrónica') iconName = 'devices';
-          else if (route.name === 'Hogar') iconName = 'chair';
-          else iconName = 'help';
-          return <Icon name={iconName} size={size} color={color} />;
-        },
-        tabBarActiveTintColor: '#6200ee',
-        tabBarInactiveTintColor: '#888',
-        headerShown: false,
-      })}
-    >
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Buscar" component={BusquedaScreen} />
-      <Tab.Screen name="Ropa" component={RopaScreen} />
-      <Tab.Screen name="Electrónica" component={ElectronicaScreen} />
-      <Tab.Screen name="Hogar" component={HogarScreen} />
-    </Tab.Navigator>
+    <Stack.Navigator initialRouteName="Inicio">
+      <Stack.Screen name="Inicio" component={HomeScreen} />
+      <Stack.Screen name="Ropa" component={RopaScreen} />
+      <Stack.Screen name="Electrónica" component={ElectronicaScreen} />
+      <Stack.Screen name="Hogar" component={HogarScreen} />
+      <Stack.Screen name="Buscar" component={BusquedaScreen} />
+    </Stack.Navigator>
   );
 };
 
